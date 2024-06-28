8a145d7fa95bb37cb5da39bcb07387d0f7f2a30f
Hari Narayana Rath <119601501+Hari-Narayana-Rath@users.noreply.github.com>
2024-06-29 03:09:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,12 @@ import pickle
 import pandas as pd
 
 def predict_sentiment(text, sia_model):
-    """Predicts the sentiment (positive or negative) of the given text using the loaded model.
-
-    Args:
-        text (str): The text to analyze for sentiment.
-        sia_model (object): The loaded sentiment analysis model.
-
-    Returns:
-        str: The predicted sentiment (positive or negative).
-    """
 
     prediction = sia_model.polarity_scores(text)
     if prediction['pos'] > prediction['neg']:
-        return "Positive"
+        return "Positive 😁"
     else:
-        return "Negative"
+        return "Negative 😔"
 
 def load_sentiment_model():
     """Loads the sentiment analysis model from the pickle file (sia.pkl).
@@ -33,14 +24,18 @@ def load_sentiment_model():
 def main():
     """Main function to handle Streamlit app interactions."""
 
-    st.title("Sentiment Analysis App")
+    st.title("Sentiment Analysis🎭")
 
-    user_input = st.text_input("Enter text to analyze:")
+    user_input = st.text_input("Enter something NEGATIVE or POSITIVE to analyze:")
 
     if user_input:
         sia_model = load_sentiment_model()  # Load model only when needed
         prediction = predict_sentiment(user_input, sia_model)
         st.write(f"Sentiment: {prediction}")
+
+    # Footer
+    st.markdown("---")
+    st.markdown("Made with ❤️ by RAJ")
 
 if __name__ == '__main__':
     main()
